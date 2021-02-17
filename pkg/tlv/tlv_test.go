a9c664a431d1fc48f226397002e53e8442616197
noah.bloom <noah.bloom@everactive.com>
2021-02-16 23:16:39 -0500

    have tests for tlv

--- pkg/tlv/tlv_test.go
+++ pkg/tlv/tlv_test.go
@@ -28,9 +28,34 @@ func TestStaticVals(t *testing.T) {
 }
 
 func TestValToInt(t *testing.T) {
-	temp1 := []byte{49, 49, 49, 50}
+	// positives
+	temp1 := []byte{49, 52, 48, 48, 49, 50}
 	exp1 := 18
 	if !verifyValToInt(temp1, exp1) {
+		t.Fail()
+	}
+	temp2 := []byte{49, 52, 48, 48, 49, 57}
+	exp2 := 25
+	if !verifyValToInt(temp2, exp2) {
+		t.Fail()
+	}
+	temp3 := []byte{49, 52, 48, 48, 49, 65}
+	exp3 := 26
+	if !verifyValToInt(temp3, exp3) {
+		t.Fail()
+	}
+
+	// zero
+	temp4 := []byte{49, 52, 48, 48, 48, 48}
+	exp4 := 0
+	if !verifyValToInt(temp4, exp4) {
+		t.Fail()
+	}
+
+	// negatives
+	temp5 := []byte{49, 52, 70, 70, 69, 55}
+	exp5 := -24
+	if !verifyValToInt(temp5, exp5) {
 		t.Fail()
 	}
 }
