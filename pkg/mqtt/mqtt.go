8f7189d7cb1fba02169b5e48586acc160202343a
noah.bloom <noah.bloom@everactive.com>
2021-02-20 09:41:53 -0500

    adding boilerplate mqtt stuff, starting on TDD

--- pkg/mqtt/mqtt.go
+++ pkg/mqtt/mqtt.go
@@ -1,4 +1,30 @@
 // Package mqtt sends data packets to a cloud server using the MQTT protocol
 package mqtt
 
+import (
+	"fmt"
 
+	"github.com/sirupsen/logrus"
+
+	paho "github.com/eclipse/paho.mqtt.golang"
+)
+
+// Connection is the entrypoint for all things MQTT
+type Connection struct {
+	Options *paho.ClientOptions
+	Client  *paho.Client
+}
+
+// NewConnection creates a new MQTT connection or error
+func NewConnection(scheme string, broker string, port int) *Connection {
+	server := fmt.Sprintf("%s://%s:%d", scheme, broker, port)
+	logrus.Info("opening MQTT connection at %s", server)
+	options := paho.NewClientOptions()
+	options.AddBroker(server)
+	client := paho.NewClient(options)
+	conn := Connection{
+		options,
+		&client,
+	}
+	return &conn
+}
--- pkg/mqtt/mqtt_test.go
+++ pkg/mqtt/mqtt_test.go
@@ -1,6 +1,15 @@
 package mqtt_test
 
-import "testing"
+import (
+	"testing"
+
+	_ "github.com/ntbloom/rainbase/pkg/mqtt"
+	_ "github.com/spf13/viper"
+)
+
+func TestMain(m *testing.M) {
+	// get config from viper
+}
 
 // can we communicate with the cloud?
 func TestMQTTConnection(t *testing.T) {
