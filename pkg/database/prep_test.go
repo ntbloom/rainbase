7eab3f01e8ef74fb40661514d7fa392b2dfc2093
noah.bloom <noah.bloom@everactive.com>
2021-02-25 23:32:07 -0500

    ready to start writing/figuring out sqlite with go

--- pkg/database/prep_test.go
+++ pkg/database/prep_test.go
@@ -1,21 +1,16 @@
 package database_test
 
 import (
-	"os"
 	"testing"
 
 	"github.com/ntbloom/rainbase/pkg/database"
 	"github.com/sirupsen/logrus"
 )
 
-func TestDBCreation(t *testing.T) {
-	file := database.SqliteDev
-	err := os.Remove(file)
+func TestDBPrep(t *testing.T) {
+	_, err := database.NewSqliteFile(database.SqliteDev)
 	if err != nil {
-		logrus.Errorf("can't remove %s: %s", file, err)
-	}
-	_, err = os.Stat(database.SqliteDev)
-	if err != nil {
+		logrus.Errorf("failing on NewSqliteFile")
 		t.Fail()
 	}
 }
