84bfcdee2d72ab02c232f2fadab6afebfc8b17ca
ntbloom <ntbloom@gmail.com>
2021-03-16 22:32:10 -0400

    added abstraction layer in front of sqlite

--- pkg/database/database_test.go
+++ pkg/database/database_test.go
@@ -31,49 +31,14 @@ func sqliteConnectionFixture() *database.DBConnector {
 	return db
 }
 
-/* TESTS */
-
-// create and destroy sqlite file 5 times, get DBCOnnector struct
-func TestDatabasePrep(t *testing.T) {
-	getConfig()
-	sqliteFile := viper.GetString(configkey.DatabaseLocalDevFile)
-
-	// clean up when finished
-	defer func() { _ = os.Remove(sqliteFile) }()
-
-	// create and destroy 5 times
-	for i := 0; i < 5; i++ {
-		db, err := database.NewSqliteDBConnector(sqliteFile, true)
-		if err != nil || db == nil {
-			logrus.Error("database not created")
-			t.Error(err)
-		}
-		_, err = os.Stat(sqliteFile)
-		if err != nil {
-			logrus.Error("sqlite file doesn't exist")
-			t.Error(err)
-		}
-	}
-}
-
-// make sure foreign key contraints are enforced
-func TestForeignKeysEnforced(t *testing.T) {
-	db := sqliteConnectionFixture()
-	if foreignKeys := db.ForeignKeysAreImplemented(); !foreignKeys {
-		logrus.Error("sqlite is not enforcing foreign_key constraints")
-		t.Fail()
-	}
-}
-
 // Property-based test for creating a bunch of rows and making sure the data get put in
-func TestRainEntry(t *testing.T) {
+func testRainEntry(db *database.DBConnector, t *testing.T) {
 	maxCount := 5
 	if testing.Short() {
 		logrus.Info("skipping property tests")
 		return
 	}
 
-	db := sqliteConnectionFixture()
 	var total int
 	test := func(reps uint8) bool {
 		count := int(reps)
@@ -97,8 +62,7 @@ func TestRainEntry(t *testing.T) {
 }
 
 // Tests all the various entries work (except temperature). Also tests concurrent use of database
-func TestStaticSQLEntries(t *testing.T) {
-	db := sqliteConnectionFixture()
+func testStaticSQLEntries(db *database.DBConnector, t *testing.T) {
 	count := 5
 
 	// asynchronously make an entry for each type
@@ -138,8 +102,7 @@ func TestStaticSQLEntries(t *testing.T) {
 }
 
 // tests that we can enter temperature
-func TestTemperatureEntries(t *testing.T) {
-	db := sqliteConnectionFixture()
+func testTemperatureEntries(db *database.DBConnector, t *testing.T) {
 	vals := []int{-100, -25, -15, -1, 0, 1, 2, 20, 24, 100}
 	for _, expected := range vals {
 		db.MakeTemperatureEntry(expected)
@@ -148,4 +111,54 @@ func TestTemperatureEntries(t *testing.T) {
 			t.Fail()
 		}
 	}
+}
+
+/* TESTS */
+
+/* Starting with Sqlite, make sure the schema and file manipulation are enforced properly */
+
+// create and destroy sqlite file 5 times, get DBCOnnector struct
+func TestSqliteDataPrep(t *testing.T) {
+	getConfig()
+	sqliteFile := viper.GetString(configkey.DatabaseLocalDevFile)
+
+	// clean up when finished
+	defer func() { _ = os.Remove(sqliteFile) }()
+
+	// create and destroy 5 times
+	for i := 0; i < 5; i++ {
+		db, err := database.NewSqliteDBConnector(sqliteFile, true)
+		if err != nil || db == nil {
+			logrus.Error("database not created")
+			t.Error(err)
+		}
+		_, err = os.Stat(sqliteFile)
+		if err != nil {
+			logrus.Error("sqlite file doesn't exist")
+			t.Error(err)
+		}
+	}
+}
+
+func TestSqliteForeignKeysAreImplemented(t *testing.T) {
+	db := sqliteConnectionFixture()
+	if foreignKeys := db.ForeignKeysAreImplemented(); !foreignKeys {
+		logrus.Error("sqlite is not enforcing foreign_key constraints")
+		t.Fail()
+	}
+}
+
+func TestSqliteRainEntry(t *testing.T) {
+	db := sqliteConnectionFixture()
+	testRainEntry(db, t)
+}
+
+func TestSqliteStaticSQLEntries(t *testing.T) {
+	db := sqliteConnectionFixture()
+	testStaticSQLEntries(db, t)
+}
+
+func TestSqliteTemperatureEntries(t *testing.T) {
+	db := sqliteConnectionFixture()
+	testTemperatureEntries(db, t)
 }
